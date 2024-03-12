001af1e076ede75c204d7fdc91a87faad6ffd8bd
channel1110 <kim1110kr@naver.com>
2024-03-12 21:25:20 +0900

    App.js Component 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="gray-background">
       <img src={logo} lat="logo"/>
       <h2>Let's develop managerment system!</h2>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
