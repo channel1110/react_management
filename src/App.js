98b60c24e379091a88978f9c8cd0430cc783500a
channel1110 <kim1110kr@naver.com>
2024-03-18 23:27:37 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,85 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Customer from './components/Custormer';
-import './App.css';
+import Customer from './components/Customer';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import Paper from '@mui/material/Paper';
+import { withStyles } from '@mui/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+});
 
 const customers = [
-{
-  'id': 1,
-  'image' : 'https://picsum.photos/64/64/?img=1',
-  'name' : '김누구',
-  'birthday' : '030808',
-  'gender' : '남자',
-  'job' : '대학생'
-},
-{
-  'id': 2,
-  'image' : 'https://picsum.photos/64/64?img=2',
-  'name' : '갑',
-  'birthday' : '880901',
-  'gender' : '남자',
-  'job' : '회사원'
-},
+  {
+    'id': 1,
+    'image' : 'https://picsum.photos/64/64/?img=1',
+    'name' : '김누구',
+    'birthday' : '030808',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id': 2,
+    'image' : 'https://picsum.photos/64/64?img=2',
+    'name' : '갑',
+    'birthday' : '880901',
+    'gender' : '남자',
+    'job' : '회사원'
+  },
+  {
+    'id': 3,
+    'image' : 'https://picsum.photos/64/64?img=3',
+    'name' : '을',
+    'birthday' : '950523',
+    'gender' : '남자',
+    'job' : '자영업자'
+  }
+];
 
-{
-  'id': 3,
-  'image' : 'https://picsum.photos/64/64?img=3',
-  'name' : '을',
-  'birthday' : '950523',
-  'gender' : '남자',
-  'job' : '자영업자'
-}
-]
 class App extends Component {
   render() {
-  return (
-    <div>
-      {/* <Customer
-        id={customers[0].id}
-        image={customers[0].image}
-        name={customers[0].name}
-        birthday={customers[0].birthday}
-        gender={customers[0].gender}
-        job={customers[0].job}
-      />
-      <Customer
-        id={customers[1].id}
-        image={customers[1].image}
-        name={customers[1].name}
-        birthday={customers[1].birthday}
-        gender={customers[1].gender}
-        job={customers[1].job}
-      />
-      <Customer
-        id={customers[2].id}
-        image={customers[2].image}
-        name={customers[2].name}
-        birthday={customers[2].birthday}
-        gender={customers[2].gender}
-        job={customers[2].job}
-      /> */}
-    {
-      customers.map(c => {
-        return(
-          <Customer
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gender={c.gender}
-            job={c.job}
-          />
-        )
-      })
-    }
-
-    </div>
-
-
-
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}> 
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => { return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>)})}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
